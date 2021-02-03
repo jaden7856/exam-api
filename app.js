8c0fe891ce15e68a20aabf305d7043a021e0d0d8
Jaden7856 <jhg7856@gmail.com>
2021-02-03 17:09:45 +0900

    Exam-api

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+
+// http://127.0.0.1:3000/
+app.get('/', (req, res) => res.send('Hello, Nodejs!'));
+app.get('/welcome', (req, res) => {
+    var status = req.param('status');
+    res.send('The system is running. [Status: ' + status + ']');
+});
+
+app.listen(port, () => console.log('Example app listening on port 3000'))
